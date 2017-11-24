34bac7b6ebcd14890499b7264b0790ef1c27441b
Matt Loupas <Matt Loupas>
2017-11-24 01:59:25 -0500

    easy340 created

---
+++ src/main/io/loupas/dailyprogramer/easy/n340/RecurringCharacter.java
@@ -0,0 +1,31 @@
+package io.loupas.dailyprogramer.easy.n340;
+
+import java.util.HashMap;
+
+public class RecurringCharacter {
+	
+	public Character character;
+	public int index;
+	
+	public static RecurringCharacter getRecurringCharacter(String input){
+		return new RecurringCharacter(input);
+	}
+	
+	public RecurringCharacter(String input){
+		this.findRecurringCharacter(input);
+	}
+	
+	private void findRecurringCharacter(String input){
+		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+		int index = 0;
+		for(char c:input.toCharArray()){
+			if(map.containsKey(c)){
+				this.character = c;
+				this.index = map.get(c);
+				return;
+			} else {
+				map.put(c, index++);
+			}
+		}
+	}
+}
---
+++ src/test/io/loupas/dailyprogrammer/easy/n340/RecurringCharacterTest.java
@@ -0,0 +1,36 @@
+package io.loupas.dailyprogrammer.easy.n340;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import io.loupas.dailyprogramer.easy.n340.RecurringCharacter;
+
+public class RecurringCharacterTest {
+	@Test
+	public void test1(){
+		String input = "ABCDEBC";
+		Assert.assertEquals(new Character('B'), RecurringCharacter.getRecurringCharacter(input).character);
+		Assert.assertEquals(1, RecurringCharacter.getRecurringCharacter(input).index);
+	}
+	
+	@Test
+	public void test2(){
+		String input = "IKEUNFUVFV";
+		Assert.assertEquals(new Character('U'), RecurringCharacter.getRecurringCharacter(input).character);
+		Assert.assertEquals(3, RecurringCharacter.getRecurringCharacter(input).index);
+	}
+	
+	@Test
+	public void test3(){
+		String input = "PXLJOUDJVZGQHLBHGXIW";
+		Assert.assertEquals(new Character('J'), RecurringCharacter.getRecurringCharacter(input).character);
+		Assert.assertEquals(3, RecurringCharacter.getRecurringCharacter(input).index);
+	}
+	
+	@Test
+	public void test4(){
+		String input = "*l1J?)yn%R[}9~1\"=k7]9;0[$";
+		Assert.assertEquals(new Character('1'), RecurringCharacter.getRecurringCharacter(input).character);
+		Assert.assertEquals(2, RecurringCharacter.getRecurringCharacter(input).index);
+	}
+}
